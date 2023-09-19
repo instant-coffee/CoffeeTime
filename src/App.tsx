24dda60e4f4384752e07f742a154e0676035a19c
Lachlan MacKenzie <lachlanrm@gmail.com>
2023-09-19 08:46:12 -0700

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import "./styles.css";
+import { CoffeeTime } from "./Components/CoffeeTime/CoffeeTime";
+
+export default function App() {
+  return (
+    <div className="App">
+      <CoffeeTime />
+    </div>
+  );
+}
---
+++ src/Components/CoffeeTime/CoffeeTime.tsx
@@ -0,0 +1,3 @@
+const CoffeeTime = () => <h1>CoffeeTime</h1>;
+
+export { CoffeeTime };
